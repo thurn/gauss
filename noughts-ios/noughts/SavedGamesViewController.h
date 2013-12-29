b944a75d0b0bd684ce2476c65acacedb518e81cb
Derek Thurn <derek@fake.email>
2013-12-29 07:01:32 -0500

    saved games screen

---
+++ noughts-ios/noughts/SavedGamesViewController.h
@@ -0,0 +1,13 @@
+//
+//  SavedGamesViewController.h
+//  noughts
+//
+//  Created by Derek Thurn on 12/28/13.
+//  Copyright (c) 2013 Derek Thurn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SavedGamesViewController : UITableViewController
+
+@end
