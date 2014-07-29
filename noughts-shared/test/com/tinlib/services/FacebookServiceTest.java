a92b89c6e65e69e83e1a94cf80a9560d0c8aa612
Derek Thurn <derek@fake.email>
2014-07-29 08:46:29 -0700

    more facebook upgrade tests

--- noughts-shared/test/com/tinlib/services/FacebookServiceTest.java
+++ noughts-shared/test/com/tinlib/services/FacebookServiceTest.java
@@ -10,6 +10,7 @@ import com.tinlib.generated.Action;
 import com.tinlib.generated.Game;
 import com.tinlib.message.Subscriber0;
 import com.tinlib.message.Subscriber1;
+import com.tinlib.test.ErroringFirebase;
 import com.tinlib.test.TestHelper;
 import com.tinlib.test.TestUtils;
 import com.tinlib.test.TinTestCase;
@@ -40,13 +41,7 @@ public class FacebookServiceTest extends TinTestCase {
     final Game testGame = TestUtils.newGameWithTwoPlayers(VIEWER_ID, GAME_ID).build();
     final Action testAction = TestUtils.newUnsubmittedActionWithCommand(GAME_ID).build();
     final Firebase firebase = new Firebase(TestHelper.FIREBASE_URL);
-    TestHelper.Builder builder = TestHelper.newBuilder(this);
-    builder.setAnalyticsHandler(mockAnalyticsHandler);
-    builder.setAnonymousViewer(VIEWER_ID, VIEWER_KEY);
-    builder.setGame(testGame);
-    builder.setCurrentAction(testAction);
-    builder.setFirebase(firebase);
-    builder.setTimeService(mockTimeService);
+    TestHelper.Builder builder = newTestHelper(firebase, testGame, testAction);
     builder.runTest(new TestHelper.Test() {
       @Override
       public void run(final TestHelper helper) {
@@ -95,5 +90,57 @@ public class FacebookServiceTest extends TinTestCase {
     endAsyncTestBlock();
 
     TestHelper.verifyTrackedEvent(mockAnalyticsHandler);
+  }
+
+  @Test
+  public void testUpgradeToFacebookFirebaseGamesError() {
+    runTestUpgradeToFacebookError(new ErroringFirebase(TestHelper.FIREBASE_URL,
+        "firebaseio-demo.com/games/" + GAME_ID, "runTransaction"));
+  }
+
+  @Test
+  public void testUpgradeToFacebookFirebaseActionError() {
+    runTestUpgradeToFacebookError(new ErroringFirebase(TestHelper.FIREBASE_URL,
+        "firebaseio-demo.com/users/" + VIEWER_KEY + "/games", "addListenerForSingleValueEvent"));
+  }
+
+  @Test
+  public void testUpgradeToFacebookFirebaseFacebookActionError() {
+    runTestUpgradeToFacebookError(new ErroringFirebase(TestHelper.FIREBASE_URL,
+        "firebaseio-demo.com/facebookUsers/" + FACEBOOK_ID + "/games",
+        "setValue"));
+  }
+
+  private void runTestUpgradeToFacebookError(Firebase firebase) {
+    beginAsyncTestBlock();
+    final Game testGame = TestUtils.newGameWithTwoPlayers(VIEWER_ID, GAME_ID).build();
+    final Action testAction = TestUtils.newUnsubmittedActionWithCommand(GAME_ID).build();
+    TestHelper.Builder builder = newTestHelper(firebase, testGame, testAction);
+    builder.setErrorHandler(FINISHED_ERROR_HANDLER);
+    builder.runTest(new TestHelper.Test() {
+      @Override
+      public void run(final TestHelper helper) {
+        GameListService gameListService = new GameListService(helper.injector());
+        helper.bus().once(TinMessages.GAME_LIST_ADD, new Subscriber0() {
+          @Override
+          public void onMessage() {
+            FacebookService facebookService = new FacebookService(helper.injector());
+            facebookService.upgradeAccountToFacebook(FACEBOOK_ID);
+          }
+        });
+      }
+    });
+    endAsyncTestBlock();
+  }
+
+  private TestHelper.Builder newTestHelper(Firebase firebase, Game game, Action action) {
+    TestHelper.Builder builder = TestHelper.newBuilder(this);
+    builder.setAnalyticsHandler(mockAnalyticsHandler);
+    builder.setAnonymousViewer(VIEWER_ID, VIEWER_KEY);
+    builder.setGame(game);
+    builder.setCurrentAction(action);
+    builder.setTimeService(mockTimeService);
+    builder.setFirebase(firebase);
+    return builder;
   }
 }
