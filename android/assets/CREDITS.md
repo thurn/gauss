32f3bf536ce1388109aa1b73213db7ad0b959e4d
Derek Thurn <derek@fake.email>
2014-10-29 23:28:07 -0700

    libgdx!

---
+++ android/assets/CREDITS.md
@@ -0,0 +1,3 @@
+# Asset Credits
+
+* Gem Icons: From [PoufPoufProduction](https://github.com/PoufPoufProduction/SVG-Inventory-Icons) on github, used under the [LGPLv3 license](https://www.gnu.org/licenses/lgpl.html).
---
+++ android/src/com/tinlib/dropswitch/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tinlib.dropswitch.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tinlib.dropswitch.DropswitchMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DropswitchMain(), config);
+	}
+}
