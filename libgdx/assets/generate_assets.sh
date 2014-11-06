b534a9d2462abc9a054c11c830915449821278f6
Derek Thurn <derek@fake.email>
2014-11-06 11:51:10 -0500

    more web tests

--- libgdx/assets/generate_assets.sh
+++ libgdx/assets/generate_assets.sh
@@ -41,3 +41,16 @@ $inkscape -d 156 -e ../android/assets/1040p/background.png background.svg
 $inkscape -d 162 -e ../android/assets/1080p/background.png background.svg
 $inkscape -d 177.1764706 -e ../android/assets/1181p/background.png background.svg
 $inkscape -d 324 -e ../android/assets/2160p/background.png background.svg
+
+$inkscape -d 60 -e ../android/assets/400p/tiger.png tiger.svg
+$inkscape -d 90 -e ../android/assets/600p/tiger.png tiger.svg
+$inkscape -d 96 -e ../android/assets/640p/tiger.png tiger.svg
+$inkscape -d 106.5 -e ../android/assets/710p/tiger.png tiger.svg
+$inkscape -d 112.5 -e ../android/assets/750p/tiger.png tiger.svg
+$inkscape -d 132 -e ../android/assets/880p/tiger.png tiger.svg
+$inkscape -d 150.0882353 -e ../android/assets/1001p/tiger.png tiger.svg
+$inkscape -d 150.75 -e ../android/assets/1005p/tiger.png tiger.svg
+$inkscape -d 156 -e ../android/assets/1040p/tiger.png tiger.svg
+$inkscape -d 162 -e ../android/assets/1080p/tiger.png tiger.svg
+$inkscape -d 177.1764706 -e ../android/assets/1181p/tiger.png tiger.svg
+$inkscape -d 324 -e ../android/assets/2160p/tiger.png tiger.svg
