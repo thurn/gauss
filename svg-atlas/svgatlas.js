b783317509a3fc64629d088cdd629ba22b1f37ca
Derek Thurn <derek@fake.email>
2014-11-08 11:52:54 -0500

    svg-atlas

---
+++ svg-atlas/svgatlas.js
@@ -0,0 +1,87 @@
+var svgatlas = {};
+
+svgatlas.SvgAtlasLoader = function(url, crossorigin) {
+  PIXI.EventTarget.call(this);
+  this.url = url;
+};
+
+// constructor
+svgatlas.SvgAtlasLoader.prototype.constructor = svgatlas.SvgAtlasLoader;
+
+svgatlas.SvgAtlasLoader.prototype.load = function() {
+  var scope = this;
+
+  if (window.XDomainRequest && scope.crossorigin) {
+    this.ajaxRequest = new window.XDomainRequest();
+    this.ajaxRequest.timeout = 3000;
+
+    this.ajaxRequest.onerror = function () {
+      scope.onError();
+    };
+    this.ajaxRequest.ontimeout = function () {
+      scope.onError();
+    };
+    this.ajaxRequest.onprogress = function() {};
+  } else if (window.XMLHttpRequest) {
+    this.ajaxRequest = new window.XMLHttpRequest();
+  } else {
+    this.ajaxRequest = new window.ActiveXObject('Microsoft.XMLHTTP');
+  }
+
+  this.ajaxRequest.onload = function(){
+    scope.onJSONLoaded();
+  };
+
+  this.ajaxRequest.open('GET',this.url,true);
+  this.ajaxRequest.send();
+};
+
+svgatlas.SvgAtlasLoader.prototype.onJSONLoaded = function() {
+  if(!this.ajaxRequest.responseText ) {
+    this.onError();
+    return;
+  }
+
+  var json = JSON.parse(this.ajaxRequest.responseText);
+  var filename = json['file'];
+  this.assets = json['assets'];
+  var image = new Image();
+  image.src = filename;
+  image.onload = this.onImageLoaded.bind(this, image);
+};
+
+svgatlas.SvgAtlasLoader.prototype.onImageLoaded = function(image) {
+  var canvas = document.createElement('canvas');
+  canvas.height = image.height;
+  canvas.width = image.width;
+  document.body.appendChild(canvas);
+  var context = canvas.getContext('2d');
+  context.drawImage(image, 0, 0, image.width, image.height);
+
+  var texture = PIXI.Texture.fromCanvas(canvas);
+  for (var i = 0; i < this.assets.length; ++i) {
+    var asset = this.assets[i];
+    var name = asset['name'];
+    var yCoordinate = image.height - asset['height'] - asset['y'];
+    PIXI.TextureCache[name] = new PIXI.Texture(texture, {
+      x: asset['x'],
+      y: yCoordinate,
+      width: asset['width'],
+      height: asset['height']
+    });
+    PIXI.TextureCache[name].crop = new PIXI.Rectangle(asset['x'], yCoordinate,
+        asset['width'], asset['height']);
+  };
+
+  this.dispatchEvent({
+    type: 'loaded',
+    content: this
+  });
+};
+
+svgatlas.SvgAtlasLoader.prototype.onError = function() {
+  this.dispatchEvent({
+    type: 'error',
+    content: this
+  });
+};
